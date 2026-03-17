e8920018daec3903c8a2a5adb94c11b9a0dc2d7a
dan-te6741 <danieli96@nycstudents.net>
2026-03-17 10:18:19 -0400

    likehalfway dobne

---
+++ merchantship.py
@@ -0,0 +1,69 @@
+Merchant_Ship_Items = [
+
+    {
+        "name": "Sword",
+        "price": 10,
+        "Weapon Type": "Medium",
+        "description": "A blade of fine craft, likely an heirloom of whoever held the blade before it was pilfered from their remains."
+    },
+    {
+        "name": "Battleaxe",
+        "price": 10,
+        "Weapon Type": "Heavy",
+        "description": "A sturdy axe perfect for splitting bone."
+    },
+    {
+        "name": "Stiletto",
+        "price": 10,
+        "Weapon Type": "Light",
+        "description": "A remarkably thin blade often likened to (and used as) a toothpick."
+    },
+    {
+        "name": "Iron Spear",
+        "price": 10,
+        "Weapon Type": "Medium",
+        "description": "A spear perfect for poking at a safe distance."
+    },
+    {
+        "name": "Mace",
+        "price": 10,
+        "Weapon Type": "Medium",
+        "description": "A simple blunt weapon effective at breaking guards."
+    },
+    {  
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    },
+    {
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    },
+    {
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    },
+    {
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    },
+    {
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    },
+    {
+        "name": "Samsung 55\" 4K UHD TV",
+        "price": 429.99,
+        "Weapon Type": "Televisions",
+        "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    }, 
+]
